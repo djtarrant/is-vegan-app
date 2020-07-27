19c31676bc8e242dc879c33648ca49d8edb1e4c4
Darren Tarrant <in@djtarrant.co.uk>
2020-07-27 17:58:50 +0100

    Add the show 'Add Food' form functionality

--- src/components/FoodItemAdd.js
+++ src/components/FoodItemAdd.js
@@ -6,13 +6,34 @@ import '../index.css';
 
 // functional component
 const FoodItemAdd = () => {
-    
+    const [addFoodShow, setAddFoodShow] = useState(false); //to show the add food form
+
     const addFood = () =>{
-        alert("Add food");
+        setAddFoodShow(!addFoodShow);
     }
     
     return (
-        <div><button onClick={addFood}>Add Food</button></div>
+        <div>
+            <button onClick={addFood}>Add Food</button>
+            {addFoodShow && (
+                <div>
+                    <form>
+                        <label>Name</label>
+                        <input type = "text" name = "name" id = "name"></input>
+                        <label>Category</label>
+                        <input type = "text" name = "category" id = "category"></input>
+                        <label>Name</label>
+                        <select name = "isVegan" id = "isVegan">
+                            <option value = "1">Yes</option>
+                            <option value = "0">No</option>
+                        </select>
+                        <label>Caveats</label>
+                        <input type = "text" name = "caveats" id = "caveats"></input>
+                        <button>Add</button>
+                    </form>    
+                </div>
+            )}
+        </div>
     )
 }
 
