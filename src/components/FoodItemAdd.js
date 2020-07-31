99a9e73e559cfb4b199d38ffe17c4d77b8cf774b
Darren Tarrant <in@djtarrant.co.uk>
2020-07-31 19:41:16 +0100

    Consolidate setState functions and ensure isVegan is set properly

--- src/components/FoodItemAdd.js
+++ src/components/FoodItemAdd.js
@@ -9,32 +9,57 @@ const FoodItemAdd = () => {
     //TODO - need to put this in a central place and pass via props - but doesn't work in Axios?
     const url = `http://localhost:5000/foodItem/`;
     const [addFoodShow, setAddFoodShow] = useState(false); //to show the add food form
-    const [name, setName] = useState("");
-    const [isVegan, setIsVegan] = useState("");
-    const [caveats, setCaveats] = useState(""); 
+    //const [name, setName] = useState("");
+    //const [caveats, setCaveats] = useState("");
+    const [isVegan, setIsVegan] = useState(""); 
+    let BooleanIsVegan = "";
+    const [state, setState] = useState({
+        name: "",
+        categoryId: "",
+        caveats: ""
+    })
 
     const showForm = () =>{
         setAddFoodShow(!addFoodShow);
     }
-
-    // TODO need to work out how to consolidate these functions
-    const changeName = (e) =>{
-        setName(e.target.value);
+    function handleChange(e){
+        const value = e.target.value;
+        setState({
+            ...state, //spreading - merging original state to new one
+            [e.target.name]: value
+        })
     }
     const changeIsVegan = (e) =>{
-        const isVegan = e.target.value? true: false;
-        setIsVegan(isVegan);
+        //select boxes only store numbers or strings in jsx
+        setIsVegan(parseInt(e.target.value));
+    }
+    /*
+    const changeName = (e) =>{
+        setName(e.target.value);
     }
     const changeCaveats = (e) =>{
         setCaveats(e.target.value);
     }
+    */
+
+
     const addFood = async (e) =>{
         e.preventDefault();
+        if(isVegan===1){
+            BooleanIsVegan=true;
+            console.log('here1');
+        }else if(isVegan===0){
+            BooleanIsVegan=false;
+            console.log('here2');
+        }else{
+            BooleanIsVegan="";
+            console.log('here3');
+        }
         var params = {
-            "name":name, 
-            "isVegan":isVegan, 
-            "caveats":caveats, 
-            "categoryId":"1", 
+            "name":state.name, 
+            "isVegan":BooleanIsVegan, 
+            "caveats":state.caveats, 
+            "categoryId":state.categoryId, 
             "isApprovedItem":true, 
             "isApprovedData":true
         };
@@ -56,17 +81,18 @@ const FoodItemAdd = () => {
             {addFoodShow && (
                 <div>
                     <form onSubmit={addFood}>
-                        <label>Name</label>
-                        <input type = "text" name = "name" id = "name" value={name} onChange={changeName}></input>
-                        <label>Category</label>
-                        <input type = "text" name = "category" id = "category"></input>
-                        <label>Vegan?</label>
+                        <label htmlFor='name'>Name</label>
+                        <input type = "text" name = "name" id = "name" value={state.name} onChange={handleChange}></input>
+                        <label htmlFor='categoryId'>Category</label>
+                        <input type = "text" name = "categoryId" id = "categoryId" value={state.categoryId} onChange={handleChange}></input>
+                        <label htmlFor='isVegan'>Vegan?</label>
                         <select name = "isVegan" id = "isVegan" value={isVegan} onChange={changeIsVegan}>
+                        <option value = "">Choose</option>
                             <option value = "1">Yes</option>
                             <option value = "0">No</option>
                         </select>
-                        <label>Caveats</label>
-                        <input type = "text" name = "caveats" id = "caveats"  value={caveats} onChange={changeCaveats}></input>
+                        <label htmlFor='caveats'>Caveats</label>
+                        <input type = "text" name = "caveats" id = "caveats"  value={state.caveats} onChange={handleChange}></input>
                         <button>Add</button>
                     </form>    
                 </div>
